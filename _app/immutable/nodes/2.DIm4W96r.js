d8e326b7745060bc2bff5ad49e90d28dbadd3cc2
CaioIcy <caio.nardelli.m@gmail.com>
2024-05-26 23:05:54 -0300

    Updates

--- _app/immutable/nodes/2.DIm4W96r.js
+++
@@ -1,1 +0,0 @@
-import{s as t,o as e}from"../chunks/scheduler.CvWTU3uz.js";import{S as n,i as a}from"../chunks/index.B7_ZjOkC.js";import{g as r}from"../chunks/entry.DNeUj2Qk.js";import{b as i}from"../chunks/paths.BsxETgsm.js";function m(o){return e(async()=>{r(`${i}/leaderboards`)}),[]}class u extends n{constructor(s){super(),a(this,s,m,null,t,{})}}export{u as component};
