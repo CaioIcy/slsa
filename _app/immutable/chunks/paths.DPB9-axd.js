11806c34cd8acdda2d7d699d6bf6e4ab623744ed
CaioIcy <4528105+CaioIcy@users.noreply.github.com>
2024-09-10 12:36:18 -0300

    Updates

--- _app/immutable/chunks/paths.DPB9-axd.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_u15l2q)==null?void 0:s.base)??"/slsa";var a;const l=((a=globalThis.__sveltekit_u15l2q)==null?void 0:a.assets)??e;export{l as a,e as b};
--- _app/immutable/entry/start.IzecQ2wg.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CdPcbHbu.js";export{t as start};
