29ea0ae8801d92f18fb458556d0268067a60213b
CaioIcy <4528105+CaioIcy@users.noreply.github.com>
2024-09-10 17:13:46 -0300

    Updates

---
+++ _app/immutable/nodes/2.CEAszgMk.js
@@ -0,0 +1,1 @@
+import{s as t,o as e}from"../chunks/scheduler.BBINyCwK.js";import{S as n,i as a}from"../chunks/index.CWDOeH18.js";import{g as r}from"../chunks/entry.BGMeIfE1.js";import{b as i}from"../chunks/paths.fV90WctV.js";function m(o){return e(async()=>{r(`${i}/leaderboards`)}),[]}class u extends n{constructor(s){super(),a(this,s,m,null,t,{})}}export{u as component};
