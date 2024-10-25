c3f04ac6067fadefd14041f1d7bae9362eeb3c6e
CaioIcy <4528105+CaioIcy@users.noreply.github.com>
2024-10-25 10:04:26 -0300

    Updates

--- _app/immutable/nodes/2.2-RazbCP.js
+++
@@ -1,1 +0,0 @@
-import{s as t,o as e}from"../chunks/scheduler.5-OGTJjP.js";import{S as n,i as a}from"../chunks/index.U6rtkGPE.js";import{g as r}from"../chunks/entry.oV2m8y0P.js";import{b as i}from"../chunks/paths.BajrL507.js";function m(o){return e(async()=>{r(`${i}/leaderboards`)}),[]}class u extends n{constructor(s){super(),a(this,s,m,null,t,{})}}export{u as component};
